948915e78c00201915a10fad7e0b0c8cef45e58f
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-17 12:45:32 +0100

    Feature: Added a feature that shows a black data page until the user searches for a city

--- weather/views.py
+++ weather/views.py
@@ -20,6 +20,6 @@ def index(request):
     else:
         city = ''
         data = {}
-    return render(request, 'index.html', data)
+    return render(request, 'index.html', {'city': city, 'data': data })
 
 
