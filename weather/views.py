e0d53142455d05b65ea937c97f4e740153ec1ff9
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-17 12:22:21 +0100

    Successfully gotten the country code data of the city searched for the weather

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=2eb7ff8ca1303524f68cca4e89ab117c').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + 
+            str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp']) + 'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+
     else:
         city = ''
-    return render(request, 'index.html', {'city': city})
+        data = {}
+    return render(request, 'index.html', data)
+
+
